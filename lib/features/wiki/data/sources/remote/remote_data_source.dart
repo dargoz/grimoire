a2a6a18da558030fa7ddffa33f35a1a1f23d30f1
Davin Reinaldo Gozali <davin_gozali@bca.co.id>
2022-09-01 15:43:32 +0700

    refactor remote_data_source.dart to become more abstract

--- lib/features/wiki/data/sources/remote/remote_data_source.dart
+++ lib/features/wiki/data/sources/remote/remote_data_source.dart
@@ -1,14 +1,9 @@
 import './gitlab/requests/repository_tree_request.dart';
-import './gitlab/responses/commit_response.dart';
-import './gitlab/responses/file_response.dart';
-import './gitlab/responses/repository_tree_response.dart';
 
 abstract class RemoteDataSource {
-  Future<FileResponse> getRepositoryFile(
-      String projectId, String filePath, String ref);
+  Future getRepositoryFile(String projectId, String filePath, String ref);
 
-  Future<CommitResponse> getCommit(String projectId, String commitId);
+  Future getCommit(String projectId, String commitId);
 
-  Future<List<RepositoryTreeResponse>> getRepositoryTree(
-      RepositoryTreeRequest request);
+  Future getRepositoryTree(RepositoryTreeRequest request);
 }
