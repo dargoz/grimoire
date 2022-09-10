71ae5daa3e33f69ad4b3b7dbbdabb07a4dac4a96
Davin Reinaldo Gozali <davinreinaldogozali@gmail.com>
2022-09-10 16:19:20 +0700

    update get_document_use_case.dart to generate default content for type tree fileTreeEntity

--- lib/features/wiki/domain/usecases/get_document_use_case.dart
+++ lib/features/wiki/domain/usecases/get_document_use_case.dart
@@ -1,5 +1,6 @@
 import 'dart:convert' show base64, utf8;
 
+import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:grimoire/core/usecases/usecase.dart';
 import 'package:grimoire/features/wiki/domain/entities/document_entity.dart';
@@ -18,21 +19,36 @@ class GetDocumentUseCase extends UseCase<DocumentEntity, FileTreeEntity> {
 
   @override
   Future<DocumentEntity> useCase(FileTreeEntity params) async {
+    if (params.type == 'tree') params.path += '/README.md';
     params.path = params.path.replaceAll('/', '%2F');
     params.path = params.path.replaceAll('.', '%2E');
-    var document = await _wikiRepository.getDocument(params.id, params.path);
-    var contentCodeUnits = base64.decode(document.content);
-    String decodedContent = utf8.decode(contentCodeUnits);
-    document.content = decodedContent;
-    document.sections = _parseDocumentSections(decodedContent);
+
+    DocumentEntity document;
     try {
-      await _searchRepository.addDocument(document);
-    } catch (e) {
-      print(e);
+      document = await _wikiRepository.getDocument(params.id, params.path);
+      var contentCodeUnits = base64.decode(document.content);
+      String decodedContent = utf8.decode(contentCodeUnits);
+      document.content = decodedContent;
+      document.sections = _parseDocumentSections(decodedContent);
+      try {
+        await _searchRepository.addDocument(document);
+      } catch (e) {
+        print(e);
+      }
+      if (kDebugMode) {
+        print('indexing done');
+      }
+    } on DioError catch (e) {
+      if (e.response?.statusCode == 404 &&
+          (e.response?.data.toString().contains('File Not Found') ?? false)) {
+        print('error message : ${e.message}');
+
+        document = _defaultDocument(params);
+      } else {
+        rethrow;
+      }
     }
-    if (kDebugMode) {
-      print('indexing done');
-    }
+
     return document;
   }
 
@@ -41,7 +57,6 @@ class GetDocumentUseCase extends UseCase<DocumentEntity, FileTreeEntity> {
     if (content != null) {
       var contents = content.split('\n');
       for (var section in contents) {
-
         if (section.startsWith('#')) {
           var attr = section.lastIndexOf('#') + 1;
           var trimSection = section.replaceAll('#', '').trim();
@@ -51,5 +66,24 @@ class GetDocumentUseCase extends UseCase<DocumentEntity, FileTreeEntity> {
     }
 
     return sections;
+  }
+
+  DocumentEntity _defaultDocument(FileTreeEntity params) {
+    params.path = params.path.replaceAll('%2F', '/');
+    params.path = params.path.replaceAll('%2E', '.');
+    return DocumentEntity(
+        fileName: 'README.md',
+        filePath: params.path,
+        size: -1,
+        content: _generateDefaultContent(params),
+        contentSha256: "content",
+        blobId: "",
+        commitId: "",
+        executeFilemode: false);
+  }
+
+  String _generateDefaultContent(FileTreeEntity params) {
+    return '# ${params.name}\n'
+        '${params.children.toString()}';
   }
 }
