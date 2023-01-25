763d24d46a4d1c859dc88f40c4a8ca1d3466f904
Davin Reinaldo Gozali <davinreinaldogozali@gmail.com>
2023-01-25 23:31:32 +0700

    fix date utils on desktop

--- lib/features/wiki/presentation/utils/date_util.dart
+++ lib/features/wiki/presentation/utils/date_util.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
+import 'package:grimoire/core/errors/catcher.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
 import 'package:intl/intl.dart';
 
 class DateUtil {
   static String formatDate(String dateString) {
-    if (dateString.isEmpty) return '';
-    if (kDebugMode) print('locale : ${Get.deviceLocale?.toString()}');
-    var dateFormat = DateFormat.yMMMMd(Get.deviceLocale?.toString());
-    return dateFormat.format(DateTime.parse(dateString));
+    try {
+      if (dateString.isEmpty) return '';
+      if (kDebugMode) print('locale : ${Get.deviceLocale?.toString()}');
+      initializeDateFormatting(Get.deviceLocale?.toString() ?? 'en_ID', null);
+      var dateFormat = DateFormat.yMMMMd(Get.deviceLocale?.toString());
+      return dateFormat.format(DateTime.parse(dateString));
+    } catch (e) {
+      Catcher.captureException(e);
+      return dateString;
+    }
   }
 }
