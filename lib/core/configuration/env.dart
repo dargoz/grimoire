3089e825e2544b0d921288046a72411b3fa356ce
Davin Reinaldo Gozali <davinreinaldogozali@gmail.com>
2023-07-30 14:46:35 +0700

    fix envied issue

--- lib/core/configuration/env.dart
+++ lib/core/configuration/env.dart
@@ -5,11 +5,11 @@ part 'env.g.dart';
 @Envied(path: '.env')
 abstract class Env {
   @EnviedField(varName: 'GIT_ACCESS_TOKEN', obfuscate: true)
-  static final gitAccessToken = _Env.gitAccessToken;
+  static final String gitAccessToken = _Env.gitAccessToken;
 
   @EnviedField(varName: 'TYPESENSE_API_KEY', obfuscate: true)
-  static final typesenseApiKey = _Env.typesenseApiKey;
+  static final String typesenseApiKey = _Env.typesenseApiKey;
 
   @EnviedField(varName: 'TYPESENSE_URL')
-  static const typesenseUrl = _Env.typesenseUrl;
+  static const String typesenseUrl = _Env.typesenseUrl;
 }
